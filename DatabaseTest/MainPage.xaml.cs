1007ae71964a3e85b47473cf8b4a5a06dd270a9c
Brady Cassada <cassadab@msoe.edu>
2019-01-13 15:11:40 -0600

    initial commit

---
+++ DatabaseTest/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using MySql.Data.MySqlClient;
+
+namespace DatabaseTest
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            String mySqlConnectionString = "YOUR CONNECTION STRING HERE";
+            MySqlConnection con = new
+                   MySqlConnection(mySqlConnectionString);
+            try
+            {
+
+                if (con.State == System.Data.ConnectionState.Closed)
+                {
+                    con.Open();
+                    String mySqlQuery = "YOUR QUERY HERE";
+                    text.Text = "Version:" + con.ServerVersion + "\nState: " + con.State.ToString();
+                    MySqlCommand command = new MySqlCommand(mySqlQuery, con);
+                    MySqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        text.Text = reader.GetString(0);
+                    }
+
+                }
+            }
+            catch (MySqlException ex)
+            {
+                text.Text = ex.ToString();
+
+            }
+            catch (InvalidOperationException ex) {
+                text.Text = ex.ToString();
+
+            }
+            finally
+            {
+                con.Close();
+            }
+
+        }
+    }
+}
